ff35dc7a68467d7bea8a9f9cc977e560cc196187
Muhammad Hammad <hammadkhalil04@.com>
2024-03-26 02:00:47 +0500

    Updated:"New Html Page"

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 async function fetchData() {
   try {
-    const res = await fetch(
-      "https://dummyjson.com/products"
-    );
+    const res = await fetch("https://dummyjson.com/products");
     if (!res.ok) {
       throw new Error("Error occurred while fetching data");
     }
@@ -15,25 +13,31 @@ async function fetchData() {
 }
 
 async function displayData() {
-    const container = document.querySelector(".container");
+  const container = document.querySelector(".container");
   try {
     const data = await fetchData();
     data.products.forEach((element) => {
-
-    
       const card = document.createElement("div");
       card.classList.add("card");
 
       const image = document.createElement("img");
       image.src = element.thumbnail;
 
-      const names =document.createElement("h2");
-      names.textContent=element.title
+      const names = document.createElement("h2");
+      names.textContent = element.title;
 
+      const press = document.createElement("h3");
+      press.textContent = "Click Here";
       card.appendChild(names);
       card.appendChild(image);
-     
+      card.appendChild(press);
       container.appendChild(card);
+      press.addEventListener("click", function () {
+        alert("Good Choice");
+        window.location.href = "index2.html";
+        console.log(element.data);
+        return console.log(data.products);
+      });
     });
   } catch (error) {
     console.error("Error occurred:", error);
